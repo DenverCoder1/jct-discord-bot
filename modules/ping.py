65d8cda41df0d98dcd7f5fa4a3ac6df254c677c9
Abraham Murciano <abrahammurciano@gmail.com>
2021-01-07 07:05:47 +0200

    Este egg
    Included more fun responses

--- modules/ping.py
+++ modules/ping.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import random
 
 class Ping(commands.Cog):
 	def __init__(self, bot):
@@ -12,7 +13,16 @@ class Ping(commands.Cog):
 		# log in console that a ping was received
 		print('Received ping')
 
-		await ctx.send('Leave me alone, I\'m trying to sleep.')
+		responses = [
+			'Leave me alone, I\'m trying to sleep',
+			'Waaaazzzzzzzzaaaaaaaaaappppppp',
+			'Who do you think you are, puny human, to be pinging me?',
+			'Pong',
+			'Hey',
+			'This is *not* the droid you are looking for :hand_splayed:'
+		]
+
+		await ctx.send(random.choice(responses))
 
 
 # This function will be called when this extension is loaded. It is necessary to add these functions to the bot.
