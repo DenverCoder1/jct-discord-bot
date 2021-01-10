075740cf433a3039843cb712f2f540480df52eca
PSilver22 <75566318+PSilver22@users.noreply.github.com>
2021-01-10 02:30:06 +0200

    Made more user friendly and using csv instead of env..

--- bot.py
+++ bot.py
@@ -2,12 +2,14 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from discord import Intents
 
 if __name__ == "__main__":
 
     load_dotenv()
 
 	# Discord setup
+<<<<<<< Updated upstream
     TOKEN = os.getenv('DISCORD_TOKEN')
     DISCORD_GUILD = int(os.getenv('DISCORD_GUILD'))
 
@@ -39,6 +41,38 @@ if __name__ == "__main__":
                 f.write(f'Unhandled message: {args[0]}\n')
             else:
                 f.write(f'Event: {event}\nMessage: {args}\n')
+=======
+	TOKEN = os.getenv("DISCORD_TOKEN")
+	DISCORD_GUILD = int(os.getenv("DISCORD_GUILD"))
+
+	intents = Intents.default()
+	intents.members = True
+
+	client = commands.Bot('~', intents=intents)  # bot command prefix
+
+	# Get the base file names of all the files in the modules folder
+	folders = filter(
+		lambda folder: os.path.exists("modules/" + folder + "/cog.py"),
+		os.listdir("modules"),
+	)
+
+	for folder in folders:
+		client.load_extension("modules." + folder + ".cog")
+
+	@client.event
+	async def on_ready():
+		"""When discord is connected"""
+		print(f"{client.user.name} has connected to Discord!")
+
+	@client.event
+	async def on_error(event, *args, **kwargs):
+		"""When an exception is raised, log it in err.log"""
+		with open("err.log", "a") as f:
+			if event == "on_message":
+				f.write(f"Unhandled message: {args[0]}\n")
+			else:
+				f.write(f"Event: {event}\nMessage: {args}\n")
+>>>>>>> Stashed changes
 
 	# Run Discord bot
     client.run(TOKEN)
