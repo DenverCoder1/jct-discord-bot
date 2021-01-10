f7dcdcb8390c37bdef427f88b10cdb7a8b759470
PSilver22 <75566318+PSilver22@users.noreply.github.com>
2021-01-10 16:31:31 +0200

    Adds Assigned role to users, accepts new format for join command.

--- modules/new_user/new_user.py
+++ modules/new_user/new_user.py
@@ -44,7 +44,7 @@ async def change_nick(member, first_name, last_name):
 
 def get_id(name):
 	'''gets the id of an object that has the name name'''
-	with open(os.path.join("modules", "new_user", "IDs.csv") as file:
+	with open(os.path.join("modules", "new_user", "IDs.csv")) as file:
 		for line in file:
 			line = line.strip('\n')
 			(key, val) = line.split(',')
