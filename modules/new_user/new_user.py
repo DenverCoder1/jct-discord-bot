a9717f03f82b48da2fe7aa96376b56a878b40247
PSilver22 <75566318+PSilver22@users.noreply.github.com>
2021-01-10 13:05:55 +0200

    Final edits.

--- modules/new_user/new_user.py
+++ modules/new_user/new_user.py
@@ -4,7 +4,7 @@ import csv
 
 def is_unassigned(member):
 	#if the member has the unassigned role
-	return (get(member.roles, id=int(get_id('UNASSIGNED_ROLE_ID'))) != None)
+	return (get(member.roles, id=get_id('UNASSIGNED_ROLE_ID')) != None)
 
 
 async def add_role(ctx, machon, year):
@@ -12,8 +12,8 @@ async def add_role(ctx, machon, year):
 	member = ctx.author
 
 	#formatting role for env file
-	role_id = machon.upper() + '_YEAR_' + year + "_ROLE_ID" #format example: LEV_YEAR_1_ROLE_ID
-	new_role = get(member.guild.roles, id = int(get_id(role_id)))
+	role_id = machon.strip(',').upper() + '_YEAR_' + year + "_ROLE_ID" #format example: LEV_YEAR_1_ROLE_ID
+	new_role = get(member.guild.roles, id=get_id(role_id))
 
 	#check if the role was legal
 	if new_role == None:
@@ -24,24 +24,31 @@ async def add_role(ctx, machon, year):
 	print(f"Gave {new_role} to {member}")
     
 
-async def remove_unassigned(member):
+async def switch_assigned(member):
 	'''removes the unassigned role from member'''
-	role = get(member.guild.roles, id = int(get_id("UNASSIGNED_ROLE_ID")))
+	#remove unassigned role
+	role = get(member.guild.roles, id=get_id("UNASSIGNED_ROLE_ID"))
 	await member.remove_roles(role)
+
+	#add assigned role
+	role = get(member.guild.roles, id=get_id("ASSIGNED_ROLE_ID"))
+	await member.add_roles(role)
+
 	print(f"Removed Unassigned from {member}")
     
     
 async def change_nick(member, first_name, last_name):
 		'''changes the nick name of the member to their full name'''
-		name = first_name.capitalize() + ' ' + last_name.capitalize()
+		name = first_name.strip(',').capitalize() + ' ' + last_name.strip(',').capitalize()
 		await member.edit(nick = name)
 		print(f"Set {member} nick to {name}")
 
+		
 def get_id(name):
 	'''gets the id of an object that has the name name'''
-	with open(os.getcwd() +"\\modules\\new_user\\IDs.csv") as file:
+	with open("modules\\new_user\\IDs.csv") as file:
 		for line in file:
 			line = line.strip('\n')
-			(key, val) = line.split('=')
+			(key, val) = line.split(',')
 			if key == name:
-				return val
+				return int(val)
