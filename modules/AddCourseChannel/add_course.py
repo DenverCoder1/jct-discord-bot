b355eb8fc8820b42360e55e929f98f348debc7c5
Shira Pahmer <65983020+shirapahmer@users.noreply.github.com>
2021-01-23 20:41:51 +0200

    Now you can add a channel
    Anyone with permission to manage the channels can add channels now. Still more work to do.

--- modules/AddCourseChannel/add_course.py
+++ modules/AddCourseChannel/add_course.py
@@ -1,39 +1,18 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands import has_permissions
 from dotenv import load_dotenv
 import csv
 import os
 import config
 import discord.utils
 
-
-# @commands.has_role('admin')
+# @has_permissions(manage_channels=True)
 async def create_channel(ctx, channel_name):
     guild = ctx.guild
     existing_channel = discord.utils.get(guild.text_channels, name=channel_name)
     if not existing_channel:
-        await ctx.send(
-            "Creating  a   new channel:    {channel_name}"
-        )  # should {} be in quotes?
+        # should {} be in quotes?
         await guild.create_text_channel(channel_name)
+        await ctx.send("Nice! You created a new channel.")
 
-# async def give_initial_role(member: discord.Member):
-#     label = "BOT_ROLE_ID" if member.bot else "UNASSIGNED_ROLE_ID"
-#     role = get_discord_obj(member.guild.roles, label)
-#     await member.add_roles(get_discord_obj(member.guild.roles, label))
-#     print(f"Gave {role.name} to {member.name}")
-#
-#
-# def get_discord_obj(iterable, label: str):
-#     def get_id(label: str):
-#         """gets the id of an object that has the given label in the CSV file"""
-#         with open(os.path.join("modules", "new_user", "ids.csv")) as csv_file:
-#             csv_reader = csv.reader(csv_file, delimiter=",")
-#
-#             for row in csv_reader:
-#                 if row[0] == label:
-#                     return int(row[1])
-#
-#             return None
-#
-#     return discord.utils.get(iterable, id=get_id(label))
--- modules/AddCourseChannel/cog.py
+++ modules/AddCourseChannel/cog.py
@@ -1,11 +1,11 @@
 import discord
 import csv
+from discord.ext.commands import has_permissions
 import config
 from discord.ext import commands
 from modules import new_user
 from modules.AddCourseChannel import add_course
-from modules.new_user.utils import get_discord_obj
-import modules.new_user.utils
+from modules.new_user import utils
 
 
 class AddCourseChannelCog(commands.Cog):
@@ -13,34 +13,23 @@ class AddCourseChannelCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @has_permissions(manage_channels=True)
     @commands.command(name="addcourse")
     async def add_course(self, ctx: commands.Context, *args):
         # log in console that a ping was received
-        print("Received addcourse")
         # checking if user is an admin, ie has an admin ID
-        if self.get_discord_obj(ctx.guild.roles,"ADMIN_ROLE-ID") not in ctx.author.roles:
-            await ctx.send(
-                "Sorry you are not an admin, you are not allowed to add a course. Please contact an admin if you "
-                "require further assistance")
-            # log in console that a ping was received
-            print("Received addcourse inside IF")
-        else:
-            # log in console that a ping was received
-            print("Received addcourse inside ELSE")
-            await ctx.send("Please enter the name of the course you would like to create a channel for")
-            course_name = "-".join(args)  # does this correctly input name?
-            course_name.lower()
-            self.create_channel(ctx, course_name)
-            # log in console that a ping was received
-            print("Received addcourse after supposed to add")
-        print(config.token)
-    '''   if not commands.bot or discord.member.guild.role != 'admin':
-           print("Sorry you are not an admin, you are not allowed to add a course. Please contact an admin\
-                 if you require further assistance")
-       else:
-           print("Please enter the name of the course you would like to create a channel for")
-           course = ctx.args
-           self.create_channel(ctx,course) '''
+        # if utils.get_discord_obj(ctx.guild.roles, "ADMIN_ROLE-ID") not in ctx.author.roles:
+        #     await ctx.send(
+        #         "Sorry, you're not an admin, you are not allowed to add a course. Please contact an admin if you "
+        #         "require further assistance.")
+        #     # log in console that a ping was received
+        #     print("Received addcourse inside IF")
+        # else:
+        #     # log in console that a ping was received
+        course_name = "-".join(args)  # does this correctly input name?
+        course_name.lower()
+        await add_course.create_channel(ctx, course_name)
+        # log in console that a ping was received
 
 
 def setup(bot):
