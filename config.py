9e352ee908eed270476083bcd9e5539c535507c2
Abraham Murciano <abrahammurciano@gmail.com>
2021-01-25 18:09:05 +0200

    connection to db

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from dotenv.main import load_dotenv
 import os
+import psycopg2
 
 
 prefix = "++"
@@ -8,3 +9,6 @@ load_dotenv()
 
 # Discord setup
 token = os.getenv("DISCORD_TOKEN")
+
+# Connect to database
+conn = psycopg2.connect(os.getenv("DATABASE_URL"), sslmode="require")
