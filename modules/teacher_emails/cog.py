661ad6ebd825b6f2c4c1f72d385377aac721d037
Abraham Murciano <abrahammurciano@gmail.com>
2021-01-29 14:21:06 +0200

    left join when retrieving emails

--- modules/teacher_emails/cog.py
+++ modules/teacher_emails/cog.py
@@ -13,26 +13,43 @@ class TeacherEmailsCog(commands.Cog, name="Teacher Emails"):
 
 	@commands.command(name="getemail", aliases=["email", "emailof"])
 	async def get_email(self, ctx: commands.Context, *args):
-		"""This command returns the email address of the teacher you ask for
+		"""This command returns the email address of the teacher you ask for.
 
 		Usage:
 		```
 		++getemail query
 		```
 		Arguments:
-		> **query**: A string which contains the professor's name and/or any of the subjects they teach (e.g. eitan computer science)
+		> **query**: A string with the professor's name and/or any courses they teach (or their channels) (e.g. eitan computer science)
 		"""
 		profs = self.finder.search(
 			args, ctx.message.channel_mentions, ctx.channel, ctx.message.mentions
 		)
 		if not profs:
-			raise FriendlyError("The email you are looking for aught to be here... But it isn't. Perhaps the archives are incomplete.", ctx.channel)
+			raise FriendlyError(
+				"The email you are looking for aught to be here... But it isn't."
+				" Perhaps the archives are incomplete.",
+				ctx.channel,
+			)
 		else:
 			title = (
 				"**_YOU_ get an email!! _YOU_ get an email!!**\nEveryone gets an email!"
 			)
 			embed = self.embedder.gen_embed(profs)
 			await ctx.send(content=title, embed=embed)
+
+	@commands.command(name="addemail")
+	async def add_email(self, ctx: commands.Context, *args):
+		"""Add the email of a professor with this command.
+
+		Usage:
+		```
+		++addemail teacher email [course]
+		```
+		Arguments:
+		> **teacher**: The teacher's name (or @mention)
+		> **email**: One or more of the teacher's email addresses
+		"""
 
 
 # This function will be called when this extension is loaded. It is necessary to add these functions to the bot.
