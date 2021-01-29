06ad353866008cf590fa6ec1f2c89b25178d71ac
Abraham Murciano <abrahammurciano@gmail.com>
2021-01-29 13:10:40 +0200

    fixed word boundaried in keyword search

--- modules/teacher_emails/queries/search_kw.sql
+++ modules/teacher_emails/queries/search_kw.sql
@@ -3,7 +3,7 @@ from teachers
 inner join teach on teachers.id = teach.teacher
 inner join courses on teach.course = courses.id
 inner join course_abbr on courses.id = course_abbr.course
-where teachers.name ~* concat('\m', f_regexp_escape(%(kw)s), '\M')
-or teachers.surname ~* concat('\m', f_regexp_escape(%(kw)s), '\M')
-or abbreviation ~* concat('\m', f_regexp_escape(%(kw)s), '\M')
-or courses.name ~* concat('\m', f_regexp_escape(%(kw)s), '\M')
+where teachers.name ~* concat('(^|\s|\m)', f_regexp_escape(%(kw)s), '(\M|\s|$)')
+or teachers.surname ~* concat('(^|\s|\m)', f_regexp_escape(%(kw)s), '(\M|\s|$)')
+or abbreviation ~* concat('(^|\s|\m)', f_regexp_escape(%(kw)s), '(\M|\s|$)')
+or courses.name ~* concat('(^|\s|\m)', f_regexp_escape(%(kw)s), '(\M|\s|$)')
