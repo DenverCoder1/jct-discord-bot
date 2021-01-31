d3a81c68e27f9917146035ca30163381a473dfab
DenverCoder1 <jonah@freshidea.com>
2021-02-01 01:39:37 +0200

    Fixed last json instance to be a Comic object

--- modules/xkcd/xkcd_fetcher.py
+++ modules/xkcd/xkcd_fetcher.py
@@ -4,7 +4,7 @@ from .comic import Comic
 
 class XKCDFetcher:
 	def get_comic_by_id(self, comic_id: int) -> Comic:
-		"""returns the json of an xkcd comic given its id"""
+		"""returns a Comic object of an xkcd comic given its id"""
 		response = requests.get(f"https://xkcd.com/{comic_id}/info.0.json")
 		if response.status_code != 200:
 			# xkcd API did not return a 200 response code
@@ -14,7 +14,7 @@ class XKCDFetcher:
 		return Comic(comic["num"], comic["safe_title"], comic["alt"], comic["img"])
 	
 	def get_latest(self) -> Comic:
-		"""returns the json of the latest xkcd comic"""
+		"""returns a Comic object of the latest xkcd comic"""
 		response = requests.get("https://xkcd.com/info.0.json")
 		if response.status_code != 200:
 			# xkcd API did not return a 200 response code
@@ -25,11 +25,11 @@ class XKCDFetcher:
 
 	def get_latest_num(self) -> int:
 		"""returns the comic number of the latest xkcd comic"""
-		json = self.get_latest()
-		return json['num']
+		comic = self.get_latest()
+		return comic.num
 
 	def get_random(self) -> Comic:
-		"""returns the json for a random xkcd comic"""
+		"""returns a Comic object for a random xkcd comic"""
 		random.seed()
 		latest_num = self.get_latest_num()
 		random_id = random.randrange(1, latest_num + 1)
