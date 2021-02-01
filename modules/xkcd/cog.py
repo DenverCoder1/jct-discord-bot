284284cc66dbada14f2f804517ef599de365ed90
DenverCoder1 <jonah@freshidea.com>
2021-02-01 16:40:47 +0200

    Update xkcd cog name to not conflict with command

--- modules/xkcd/cog.py
+++ modules/xkcd/cog.py
@@ -4,7 +4,7 @@ from modules.error.friendly_error import FriendlyError
 from discord.ext import commands
 
 
-class XKCDCog(commands.Cog, name="xkcd"):
+class XKCDCog(commands.Cog, name="XKCD Comics"):
 	def __init__(self, bot: commands.Bot):
 		self.bot = bot
 		self.xkcd_fetcher = XKCDFetcher()
