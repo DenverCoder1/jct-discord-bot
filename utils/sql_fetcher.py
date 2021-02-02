23dd9c60db0bcbf4afd4f9b9519c12868a68b967
Abraham Murciano <abrahammurciano@gmail.com>
2021-02-02 12:48:14 +0200

    type annotations

--- utils/sql_fetcher.py
+++ utils/sql_fetcher.py
@@ -8,11 +8,11 @@ class SqlFetcher:
 		for f in [f for f in os.listdir(sql_folder) if f.endswith(".sql")]:
 			self[f]  # load all sql files on initialisation
 
-	def __getitem__(self, file_name: str):
+	def __getitem__(self, file_name: str) -> str:
 		if file_name in self.sql:
 			return self.sql[file_name]
 		self.sql[file_name] = open(self.__sql_path(file_name), "r").read()
 		return self.sql[file_name]
 
-	def __sql_path(self, file):
+	def __sql_path(self, file: str) -> str:
 		return os.path.join(self.sql_folder, file)
