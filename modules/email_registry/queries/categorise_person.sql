955e232aacb418638925190eddfb796a203f667e
Abraham Murciano <abrahammurciano@gmail.com>
2021-02-03 16:55:45 +0200

    renamed column channel_id to channel

--- modules/email_registry/queries/categorise_person.sql
+++ modules/email_registry/queries/categorise_person.sql
@@ -2,4 +2,4 @@ INSERT INTO person_category (person, category)
 VALUES (%(person_id)s,
     (SELECT id
 	    FROM categories
-		WHERE channel_id = %(channel_id)s))
+		WHERE channel = %(channel_id)s))
