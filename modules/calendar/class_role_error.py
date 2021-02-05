c574f2370ba60ff6d9f710dd5ca2507762c15972
DenverCoder1 <jonah@freshidea.com>
2021-02-05 15:39:18 +0200

    Docs and formatting

--- modules/calendar/class_role_error.py
+++ modules/calendar/class_role_error.py
@@ -1,3 +1,6 @@
 class ClassRoleError(Exception):
+	"""Exception raised when a class role is missing
+	or there are more class roles than expected."""
+
 	def __init__(self, *args):
 		self.args = args
