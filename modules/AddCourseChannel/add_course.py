abc7e5dab8d7006aba8551e36dbce146551b62e4
zabrown2000 <25807259+zabrown2000@users.noreply.github.com>
2021-02-06 20:26:10 +0200

    categories and channels
    Changed it to check if categories and/or channels exist-need to test if works

--- modules/AddCourseChannel/add_course.py
+++ modules/AddCourseChannel/add_course.py
@@ -11,19 +11,27 @@ from discord import CategoryChannel
 
 async def create_channel(ctx, channel_name, category_name):
     guild = ctx.guild
-    if category_name != "none":  # if user entered a category name
-        # existing_category = discord.utils.get(ctx.guild.categories, name=category_name)
-        # if not existing_category:  # if category doesn't exist create it
-        #     await guild.create_category_channel(category_name)
-        channels = discord.get.CategoryChannel.text_channels
-    if channel_name not in channels:
-        print("blah blah")
+    # if category_name != "none":  # if user entered a category name
+    # existing_category = discord.utils.get(ctx.guild.categories, name=category_name)
+    # if not existing_category:  # if category doesn't exist create it
+    #     await guild.create_category_channel(category_name)
+    # channels = discord.get.CategoryChannel.text_channels
+    # if channel_name not in channels:
+    #     print("blah blah")
+    if category_name not in ctx.guild.categories:
+        await guild.create_category_channel(category_name)
 
+    if channel_name in ctx.guild.text_channels:
+        await ctx.send("Sorry this channel already exists. Please try again.")
+        return
+
+    await guild.create_text_channel(channel_name, category_name)
+    await ctx.send("Nice! You created a new channel.")
     # existing_channel = discord.utils.get(guild.text_channels, name=channel_name)
     # if not existing_channel:
-    if category_name != "none":
-        await guild.create_text_channel(channel_name, category_name)
-    else:
-        await guild.create_text_channel(channel_name)
+    #     if category_name != "none":
+    #         await guild.create_text_channel(channel_name, category_name)
+    #     else:
+    #         await guild.create_text_channel(channel_name)
 
-    await ctx.send("Nice! You created a new channel.")
+    #     await ctx.send("Nice! You created a new channel.")
--- modules/AddCourseChannel/cog.py
+++ modules/AddCourseChannel/cog.py
@@ -32,13 +32,18 @@ class AddCourseChannelCog(commands.Cog):
         names = " ".join(args)
         split_names = names.split(",")
 
-        course_name = split_names[
-            0
-        ]  # "-".join(args)  # does this correctly input name?
-        course_name.lower()
-        category_name = split_names[1]  # " ".join(args)
-        category_name.upper()
-        await add_course.create_channel(ctx, course_name, category_name)
+        course_name = split_names[0]
+        update_course_name = "-".join(course_name)  # does this correctly input name?
+        update_course_name.lower()
+        category_name = split_names[1]
+        if not category_name:
+            await ctx.send("Please try again with a category name.")
+        else:
+            update_category_name = " ".join(category_name)
+            update_category_name.upper()
+            await add_course.create_channel(
+                ctx, update_course_name, update_category_name
+            )
         # log in console that a ping was received
 
 
