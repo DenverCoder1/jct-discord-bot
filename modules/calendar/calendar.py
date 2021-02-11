c77900dc5cb569da7c30211cfe743550786fe149
Jonah Lawrence <jonah@freshidea.com>
2021-02-11 23:52:05 +0200

    make methods into just fields
    Co-authored-by: Abraham Murciano <abrahammurciano@gmail.com>

--- modules/calendar/calendar.py
+++ modules/calendar/calendar.py
@@ -4,11 +4,6 @@ from typing import Dict
 class Calendar:
 	def __init__(self, details: Dict[str, str] = {}, **kwargs):
 		"""Create a calendar from a JSON object or kwargs (id and name)"""
-		self.calendar_id = kwargs.get("id", details.get("id"))
-		self.calendar_name = kwargs.get("name", details.get("summary"))
+		self.id = kwargs.get("id", details.get("id"))
+		self.name = kwargs.get("name", details.get("summary"))
 
-	def id(self):
-		return self.calendar_id
-
-	def name(self):
-		return self.calendar_name
