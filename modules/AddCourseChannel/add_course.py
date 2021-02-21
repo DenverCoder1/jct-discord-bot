9fcecef20479b3982f0e4f80f8025e200df77467
Shira Pahmer <65983020+shirapahmer@users.noreply.github.com>
2021-02-21 22:03:41 +0200

    Some changes that got it working!
    Can now add a new channel name. Still need to add functionality to accept course nicknames and delete a course

--- modules/AddCourseChannel/add_course.py
+++ modules/AddCourseChannel/add_course.py
@@ -1,55 +1,21 @@
 import discord
-from discord.ext import commands
-from discord.ext.commands import has_permissions
-from dotenv import load_dotenv
-import csv
-import os
-import config
 import discord.utils
-from discord import CategoryChannel
+from modules.error.friendly_error import FriendlyError
+from utils.utils import get_discord_obj, embed_success
 
-
-async def create_channel(ctx, channel_name, category_name):
+async def create_channel(ctx, channel_name):
     guild = ctx.guild
-    categoryFlag = False
-    channelFlag = False
-    print("category name and channel name ", category_name, " ", channel_name)
-    # if category_name != "none":  # if user entered a category name
-    # existing_category = discord.utils.get(ctx.guild.categories, name=category_name)
-    # if not existing_category:  # if category doesn't exist create it
-    #     await guild.create_category_channel(category_name)
-    # channels = discord.get.CategoryChannel.text_channels
-    # if channel_name not in channels:
-    #     print("blah blah")
+    category = get_discord_obj(guild.categories, "COURSES_CATEGORY")
 
-    #if category_name not in ctx.guild.categories:
-    for category in ctx.guild.categories:
-        print("categoryFlag and category ", categoryFlag, category)
-        print("category type is:", type(category))
-        if category == category_name:
-            categoryFlag = True
+    if discord.utils.get(category.text_channels, name=channel_name) is not None:
+        raise FriendlyError("this channel already exists. Please try again.", ctx.channel, ctx.author)
+    position = category.text_channels[-1].position+1
+
+    for channel in category.text_channels:
+        if channel.name > channel_name:
+            position = channel.position-1
             break
 
-    for channel in ctx.guild.text_channels:
-        if channel == channel_name:
-            channelFlag = True
-            break
-    if channelFlag == True:
-        await ctx.send("Sorry this channel already exists. Please try again.")
-        return
-    else:
-        if categoryFlag == False:
-            await guild.create_category_channel(category_name)
+    await category.create_text_channel(channel_name, position=position)
 
-    #NEED to try getting the category ID for the category name they passed in and try creating the channel with that instead
-    await guild.create_text_channel(channel_name, category=category_name)
-    await ctx.send("Nice! You created a new channel.")
-
-    # existing_channel = discord.utils.get(guild.text_channels, name=channel_name)
-    # if not existing_channel:
-    #     if category_name != "none":
-    #         await guild.create_text_channel(channel_name, category_name)
-    #     else:
-    #         await guild.create_text_channel(channel_name)
-
-    #     await ctx.send("Nice! You created a new channel.")
+    await ctx.send(embed=embed_success("Nice! You created a new channel."))
--- modules/AddCourseChannel/cog.py
+++ modules/AddCourseChannel/cog.py
@@ -7,7 +7,7 @@ import config
 from discord.ext import commands
 from modules import new_user
 from modules.AddCourseChannel import add_course
-from modules.new_user import utils
+# from modules.new_user import utils
 
 
 class AddCourseChannelCog(commands.Cog):
@@ -19,33 +19,16 @@ class AddCourseChannelCog(commands.Cog):
     #     await ctx.send('I do not have permission to delete this role')
     @commands.command(name="addcourse")
     async def add_course(self, ctx: commands.Context, *args):
-        # log in console that a ping was received
-        # checking if user is an admin, ie has an admin ID
-        # if utils.get_discord_obj(ctx.guild.roles, "ADMIN_ROLE-ID") not in ctx.author.roles:
-        #     await ctx.send(
-        #         "Sorry, you're not an admin, you are not allowed to add a course. Please contact an admin if you "
-        #         "require further assistance.")
-        #     # log in console that a ping was received
-        #     print("Received addcourse inside IF")
-        # else:
-        #     # log in console that a ping was received
+
         names = " ".join(args)
         split_names = names.split(",")
 
         course_name = split_names[0]
         update_course_name = course_name.strip().replace(" ", "-")
-        # update_course_name = "-".join(course_name)  # does this correctly input name?
         update_course_name.lower()
-        category_name = split_names[1]
-        if not category_name:
-            await ctx.send("Please try again with a category name.")
-        else:
-            update_category_name = "".join(category_name)
-            update_category_name.upper()
-            await add_course.create_channel(
-                ctx, update_course_name, update_category_name
-            )
-        # log in console that a ping was received
+
+        await add_course.create_channel(
+                ctx, update_course_name)
 
 
 def setup(bot):
