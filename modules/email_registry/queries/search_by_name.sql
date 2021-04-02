541f96dfb43b95f3c7c81ea88883247adb7b5b15
Abraham Murciano <abrahammurciano@gmail.com>
2021-04-02 09:27:10 +0300

    Revert "Revert "add and remove emails slash commands""

--- modules/email_registry/queries/search_by_name.sql
+++ modules/email_registry/queries/search_by_name.sql
@@ -1,4 +1,4 @@
 SELECT DISTINCT people.id
 FROM people
-WHERE name ~* concat('(^|\s|\m)', f_regexp_escape(%(word)s), '(\M|\s|$)')
-OR surname ~* concat('(^|\s|\m)', f_regexp_escape(%(word)s), '(\M|\s|$)')
+WHERE name ~* concat('(^|\s|\m)', f_regexp_escape(%(param)s), '(\M|\s|$)')
+OR surname ~* concat('(^|\s|\m)', f_regexp_escape(%(param)s), '(\M|\s|$)')
