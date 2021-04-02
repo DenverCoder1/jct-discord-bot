2e14631798019d6da05811918e2aa70f2da4b13b
Abraham Murciano <abrahammurciano@gmail.com>
2021-04-02 09:26:17 +0300

    Revert "Revert "add and remove emails slash commands""

--- modules/email_registry/person_embedder.py
+++ modules/email_registry/person_embedder.py
@@ -1,26 +1,9 @@
 from typing import Iterable
 from modules.email_registry.person import Person
-from utils import utils
 import discord
 
-# TODO: deprecated
-def gen_embed(people: Iterable[Person]) -> discord.Embed:
-	embed = discord.Embed(colour=discord.Colour.teal())
 
-	for person in people:
-		embed.add_field(
-			name=person.name,
-			value=(
-				utils.blockquote(f"{person.linked_emails()}\n{person.categories}")
-				or utils.blockquote("No info found.")
-			),
-			inline=False,
-		)
-
-	return embed
-
-
-def _gen_embed(person: Person):
+def gen_embed(person: Person):
 	return discord.Embed(
 		title=person.name,
 		description=f"{person.linked_emails()}\n{person.categories}".strip()
@@ -30,4 +13,4 @@ def _gen_embed(person: Person):
 
 
 def gen_embeds(people: Iterable[Person]):
-	return [_gen_embed(person) for person in people]
+	return [gen_embed(person) for person in people]
