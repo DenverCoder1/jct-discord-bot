e836d7bd6533c287a98014018a80028202de4e28
Abraham Murciano <abrahammurciano@gmail.com>
2021-04-04 16:51:08 +0300

    /course add and /course delete

--- modules/course_management/cog.py
+++ modules/course_management/cog.py
@@ -1,3 +1,9 @@
+from re import T
+import discord
+from discord_slash import cog_ext
+from discord_slash.context import SlashContext
+from discord_slash.model import SlashCommandOptionType
+from discord_slash.utils.manage_commands import create_option
 from utils.mention import decode_mention
 from utils.embedder import embed_success
 from discord.ext.commands import has_permissions
@@ -13,64 +19,86 @@ class CourseManagerCog(commands.Cog):
 		self.adder = CourseAdder(config.conn, config.sql_fetcher)
 		self.deleter = CourseDeleter(config.conn, config.sql_fetcher)
 
-	@commands.command(name="courses.add")
+	@cog_ext.cog_subcommand(
+		base="course",
+		name="add",
+		description="Add a new course to the database and create a channel for it.",
+		guild_ids=[config.guild_id],
+		options=[
+			create_option(
+				name="course_name",
+				description=(
+					'The full name of the course. (eg. "Advanced Object Oriented'
+					' Programming and Design")'
+				),
+				option_type=SlashCommandOptionType.STRING,
+				required=True,
+			),
+			create_option(
+				name="professors",
+				description=(
+					"A comma separated string of names of professors who teach the"
+					' course. (eg. "shahar golan, eitan")'
+				),
+				option_type=SlashCommandOptionType.STRING,
+				required=False,
+			),
+			create_option(
+				name="channel_name",
+				description=(
+					"The name of the channel. (eg. object-oriented-programming)"
+					" (default is course name)"
+				),
+				option_type=SlashCommandOptionType.STRING,
+				required=False,
+			),
+		],
+	)
 	@has_permissions(manage_channels=True)
 	async def add_course(
 		self,
-		ctx: commands.Context,
+		ctx: SlashContext,
 		course_name: str,
-		labels: str,
-		professors: str,
-		channel_name: str = "",
+		professors: str = "",
+		channel_name: str = None,
 	):
-		"""
-		Add a new course to the database and create a channel for it.
-
-		Usage:
-		```
-		++courses.add <course name> <labels> <professors> [channel name]
-		```
-		Arguments:
-		**<course name>**: The full name of the course. (eg. "Advanced Object Oriented Programming and Design")
-		**<labels>**: A space separated string of words associated with the course (eg. "oop aoopd java"). These can be used later to search for the course. No need to include words already in the course name.
-		**<professors>**: A comma separated string of names of professors who teach the course. (eg. "shahar golan, eitan")
-		**[channel name]**: The name of the channel. (eg. object-oriented-programming) (optional, default is course name)
-		"""
-		labels = labels.split()
+		await ctx.defer()
 		professors = [professor.strip() for professor in professors.split(",")]
-		if channel_name == "":
+		if not channel_name:
 			channel_name = course_name
 		channel = await self.adder.add_course(
-			ctx, course_name, labels, professors, channel_name
+			ctx, course_name, professors, channel_name
 		)
 		await ctx.send(
 			embed=embed_success("Nice! You created a course channel.", channel.mention)
 		)
 
+	@cog_ext.cog_subcommand(
+		base="course",
+		name="delete",
+		description=(
+			"Delete course from the database and delete its channel. (For discontinued"
+			" courses)."
+		),
+		guild_ids=[config.guild_id],
+		options=[
+			create_option(
+				name="channel",
+				description="Mention a course the professor teaches. (eg. #automata)",
+				option_type=SlashCommandOptionType.CHANNEL,
+				required=True,
+			),
+		],
+	)
 	@has_permissions(manage_channels=True)
-	@commands.command(name="courses.delete")
-	async def delete_course(self, ctx: commands.Context, channel_mention: str):
-		"""
-		Delete course from the database and delete its channel. Note: This should only be done for a course that will never be taught again.
-
-		Usage:
-		```
-		++courses.delete <course mention>
-		```
-		Arguments:
-		**<course mention>**: Mention the channel of the course you want to delete using #. (eg. #object-oriented-programming)
-		"""
-		mention_type, channel_id = decode_mention(channel_mention)
-		if mention_type != "channel":
-			raise FriendlyError(
-				"Expected a channel mention.",
-				description=(
-					f"For example use ```\n{config.prefix}courses.delete"
-					" #object-oriented-programming```"
-				),
+	async def delete_course(self, ctx: SlashContext, channel: discord.TextChannel):
+		await self.deleter.delete_course(ctx, channel.id)
+		await ctx.send(
+			embed=embed_success(
+				"Well done... All evidence of that course has been deleted from the"
+				" face of the earth. Hope you're proud of yourself."
 			)
-		await self.deleter.delete_course(ctx, channel_id)
-		await ctx.send(embed=embed_success("You successfully deleted the course."))
+		)
 
 
 def setup(bot):
--- modules/course_management/course_adder.py
+++ modules/course_management/course_adder.py
@@ -1,12 +1,11 @@
+from discord_slash.context import SlashContext
 import psycopg2.extensions as sql
 import discord
 import discord.utils
-import config
-from discord.ext import commands
+from psycopg2.errors import UniqueViolation
 from modules.email_registry.categoriser import Categoriser
 from modules.email_registry.person_finder import PersonFinder
 from modules.error.friendly_error import FriendlyError
-from psycopg2.extras import execute_values
 from typing import Iterable
 from utils import embedder
 from utils.sql_fetcher import SqlFetcher
@@ -22,19 +21,18 @@ class CourseAdder:
 
 	async def add_course(
 		self,
-		ctx: commands.Context,
+		ctx: SlashContext,
 		course_name: str,
-		labels: Iterable[str],
 		professors: Iterable[str],
 		channel_name: str,
 	):
 		channel = await self.__create_channel(ctx, channel_name)
-		self.__add_to_database(channel, course_name, labels)
+		await self.__add_to_database(ctx, channel, course_name)
 		await self.__link_professors(ctx, channel, professors)
 		return channel
 
 	async def __create_channel(
-		self, ctx: commands.Context, channel_name: str
+		self, ctx: SlashContext, channel_name: str
 	) -> discord.TextChannel:
 		# find courses category
 		category = get_discord_obj(ctx.guild.categories, "COURSES_CATEGORY")
@@ -42,9 +40,7 @@ class CourseAdder:
 		# make sure the channel doesn't already exist
 		if discord.utils.get(category.text_channels, name=channel_name) is not None:
 			raise FriendlyError(
-				"this channel already exists. Please try again.",
-				ctx.channel,
-				ctx.author,
+				"this channel already exists. Please try again.", ctx, ctx.author,
 			)
 
 		# create the new course channel at the bottom of the category
@@ -60,39 +56,48 @@ class CourseAdder:
 
 		return new_channel
 
-	def __add_to_database(
-		self, channel: discord.TextChannel, course_name: str, labels: Iterable[str]
+	async def __add_to_database(
+		self, ctx: SlashContext, channel: discord.TextChannel, course_name: str
 	):
 		course_query = self.sql_fetcher.fetch(
 			"modules", "course_management", "queries", "add_course.sql"
 		)
-		labels_query = self.sql_fetcher.fetch(
-			"modules", "course_management", "queries", "add_label.sql"
-		)
 		with self.conn as conn:
 			with conn.cursor() as cursor:
 				# add the course to the database
-				cursor.execute(
-					course_query, {"course_name": course_name, "channel_id": channel.id}
-				)
-				# add the labels to the course
-				execute_values(
-					cursor,
-					labels_query,
-					argslist=[{"label": label} for label in labels],
-					template=f"({cursor.fetchone()[0]}, %(label)s)",
-				)
+				try:
+					cursor.execute(
+						course_query,
+						{"course_name": course_name, "channel_id": channel.id},
+					)
+				except UniqueViolation as e:
+					await channel.delete(
+						reason=(
+							"The command that created this channel ultimately failed,"
+							" so it was deleted."
+						)
+					)
+					raise FriendlyError(
+						"A course with this name already exists in the database.",
+						description=(
+							"If a channel for this is missing, then the existing course"
+							" will have to be deleted from the database manually. Ask a"
+							" bot dev to do this."
+						),
+						sender=ctx,
+						inner=e,
+					)
 
 	async def __link_professors(
 		self,
-		ctx: commands.Context,
+		ctx: SlashContext,
 		channel: discord.TextChannel,
 		professors: Iterable[str],
 	):
 		for professor in professors:
 			professor = professor.strip()
 			try:
-				professor_id = self.finder.search_one(professor.split(), ctx.channel).id
+				professor_id = self.finder.search_one(ctx, professor).id
 				self.categoriser.categorise_person(professor_id, (channel.mention,))
 			except FriendlyError:
 				await ctx.send(
@@ -103,9 +108,8 @@ class CourseAdder:
 						),
 						description=(
 							"To link the professor yourself, first add them with"
-							f" `{config.prefix}addperson` if they're not in the"
-							f" system, then link them with\n```\n{config.prefix}link"
-							f' "{professor}" to #{channel.name}```'
+							" `/email person add` if they're not in the system, then"
+							" link them with `/email person link`"
 						),
 					)
 				)
