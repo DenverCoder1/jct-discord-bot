9c253b716f35ca5ef9eb443dc3db5b235ff36f9c
Abraham Murciano <abrahammurciano@gmail.com>
2021-04-05 00:39:30 +0300

    simplified

--- modules/graduation/cog.py
+++ modules/graduation/cog.py
@@ -1,4 +1,4 @@
-from modules.graduation.graduator import Graduator
+from modules.graduation import graduation
 from discord.ext import commands
 from utils.scheduler.scheduler import Scheduler
 import config
@@ -7,13 +7,10 @@ import config
 class GraduationCog(commands.Cog, name="Graduation"):
 	"""Performs the required housekeeping when a class graduates."""
 
-	def __init__(self):
-		self.graduator = Graduator(config.conn, config.sql_fetcher, config.guild)
-
 	@Scheduler.schedule()
 	async def on_winter_semester_start(self):
-		groups = self.graduator.get_graduating_groups()
-		await self.graduator.add_alumni_role(groups)
+		groups = graduation.get_graduating_groups()
+		await graduation.add_alumni_role(groups)
 
 
 # This function will be called when this extension is loaded. It is necessary to add these functions to the bot.
