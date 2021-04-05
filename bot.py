d0c32b7de58b265cc79bb03fda3f19fa0afcdb24
Abraham Murciano <abrahammurciano@gmail.com>
2021-04-05 06:14:41 +0300

    removed main loop

--- bot.py
+++ bot.py
@@ -12,26 +12,25 @@ def main():
 	intents.guilds = True
 	intents.members = True
 
-	while True:
-		bot = commands.Bot("/", intents=intents)  # bot command prefix
-		bot.slash = SlashCommand(bot, override_type=True, sync_commands=True)
+	bot = commands.Bot("/", intents=intents)  # bot command prefix
+	bot.slash = SlashCommand(bot, override_type=True, sync_commands=True)
 
-		# Get the modules of all cogs whose directory structure is modules/<module_name>/cog.py
-		for folder in os.listdir("modules"):
-			if os.path.exists(os.path.join("modules", folder, "cog.py")):
-				bot.load_extension(f"modules.{folder}.cog")
+	# Get the modules of all cogs whose directory structure is modules/<module_name>/cog.py
+	for folder in os.listdir("modules"):
+		if os.path.exists(os.path.join("modules", folder, "cog.py")):
+			bot.load_extension(f"modules.{folder}.cog")
 
-		@bot.event
-		async def on_ready():
-			"""When discord is connected"""
-			print(f"{bot.user.name} has connected to Discord!")
-			config._guild = bot.get_guild(config.guild_id)
-			await bot.change_presence(activity=discord.Game("with students' patience"))
-			# Start Scheduler
-			Scheduler(bot)
+	@bot.event
+	async def on_ready():
+		"""When discord is connected"""
+		print(f"{bot.user.name} has connected to Discord!")
+		config._guild = bot.get_guild(config.guild_id)
+		await bot.change_presence(activity=discord.Game("with students' patience"))
+		# Start Scheduler
+		Scheduler(bot)
 
-		# Run Discord bot
-		bot.run(config.token)
+	# Run Discord bot
+	bot.run(config.token)
 
 
 if __name__ == "__main__":
