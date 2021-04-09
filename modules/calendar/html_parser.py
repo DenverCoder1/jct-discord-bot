beb5d312ed8617259d2d415997dc63360100a4ac
Jonah Lawrence <jonah@freshidea.com>
2021-04-09 15:08:08 +0300

    partial_link in full_link won't work if partial_link is None

--- modules/calendar/html_parser.py
+++ modules/calendar/html_parser.py
@@ -21,11 +21,17 @@ def md_links_to_html(text: str) -> str:
 def html_links_to_md(text: str) -> str:
 	def repl(match):
 		"""parses link regex as a shortened markdown link with full link as title attribute"""
+		# inner text of the link or None if the text is the link
+		inner_text = match.group(4)
+		# trimmed version of the link or None if the link has inner text
 		partial_link = match.group(3) or match.group(6)
-		label = match.group(4)
+		# full href of the link
 		full_link = match.group(2) or match.group(5)
-		trimmed = partial_link in full_link and not full_link.endswith(partial_link)
-		label = label or (partial_link + ("..." if trimmed else ""))
+		# check if partial link is a trimmed version of the full link
+		trimmed = partial_link and not full_link.endswith(partial_link)
+		# set the label for the markdown link
+		label = inner_text or partial_link + ("..." if trimmed else "")
+		# return markdown link
 		return f'[{label}]({full_link} "{full_link}")'
 
 	return __HTML_LINK_REGEX.sub(repl, text)
