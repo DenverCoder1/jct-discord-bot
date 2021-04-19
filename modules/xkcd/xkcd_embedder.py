547e80df725d2fc3a1f867cc10f5d3557917f3d6
Jonah Lawrence <jonah@freshidea.com>
2021-04-19 19:41:16 +0300

    Create trim in utils

--- modules/xkcd/xkcd_embedder.py
+++ modules/xkcd/xkcd_embedder.py
@@ -1,10 +1,11 @@
+from utils.embedder import embed_success
 import discord
 from .comic import Comic
 
 
 class XKCDEmbedder:
 	def gen_embed(self, comic: Comic) -> discord.Embed:
-		embed = discord.Embed(
+		embed = embed_success(
 			title=f"{comic.num}: {comic.title}",
 			url=f"https://xkcd.com/{comic.num}",
 			colour=discord.Colour.blurple(),
