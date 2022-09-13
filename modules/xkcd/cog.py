80c78ef0d59ab905e9b15e92bfeb440526a6115f
BenjiTusk <benjitusk1@gmail.com>
2022-09-13 14:45:53 -0400

    Migrate xkcd command to nextcord, closes #369

--- modules/xkcd/cog.py
+++ modules/xkcd/cog.py
@@ -3,6 +3,7 @@ from .xkcd_fetcher import XKCDFetcher
 from .xkcd_embedder import XKCDEmbedder
 from nextcord.ext import commands
 import config
+import nextcord
 
 
 class XKCDCog(commands.Cog):
@@ -13,61 +14,65 @@ class XKCDCog(commands.Cog):
 		self.xkcd_fetcher = XKCDFetcher()
 		self.xkcd_embedder = XKCDEmbedder()
 
-	@cog_ext.cog_subcommand(
-		base="xkcd",
-		name="get",
-		description="Get a random xkcd comic or one you specify.",
-		guild_ids=[config.guild_id],
-		options=[
-			create_option(
-				name="comic_id",
-				description=(
-					"The id of the xkcd comic you want to get. (eg. 221) (default is"
-					" random)"
-				),
-				option_type=SlashCommandOptionType.INTEGER,
-				required=False,
-			)
-		],
-	)
-	async def xkcd(self, interaction: nextcord.Interaction, comic_id: Optional[int] = None):
-		await ctx.defer()
-		comic = (
-			self.xkcd_fetcher.get_comic_by_id(comic_id)
-			if comic_id
-			else self.xkcd_fetcher.get_random()
+	@nextcord.command(name="xkcd")
+	async def xkcd(self, interaction: nextcord.Interaction):
+		"""This is a base command for all xkcd commands and is not invoked"""
+		pass
+
+	@xkcd.subcommand(name="latest", description="Displays the latest xkcd comic")
+	async def latest(self, interaction: nextcord.Interaction):
+		"""Displays the latest xkcd comic"""
+		interaction.response.defer()
+		try:
+			comic = await self.xkcd_fetcher.get_latest()
+		except ConnectionError as e:
+			interaction.response.send_message(e, ephemeral=True)
+			return
+
+		await interaction.response.send_message(
+			embed=self.xkcd_embedder.gen_embed(comic)
 		)
-		await ctx.send(embed=self.xkcd_embedder.gen_embed(comic))
 
-	@cog_ext.cog_subcommand(
-		base="xkcd",
-		name="latest",
-		description="Get the latest xkcd comix.",
-		guild_ids=[config.guild_id],
-	)
-	async def xkcd_latest(self, interaction: nextcord.Interaction):
-		await ctx.defer()
-		comic = self.xkcd_fetcher.get_latest()
-		await ctx.send(embed=self.xkcd_embedder.gen_embed(comic))
+	@xkcd.subcommand(name="random", description="Displays a random xkcd comic")
+	async def random(self, interaction: nextcord.Interaction):
+		"""Displays a random xkcd comic"""
+		interaction.response.defer()
+		comic = await self.xkcd_fetcher.get_random()
+		await interaction.response.send_message(
+			embed=self.xkcd_embedder.gen_embed(comic)
+		)
 
-	@cog_ext.cog_subcommand(
-		base="xkcd",
-		name="search",
-		description="Search for an xkcd comic",
-		guild_ids=[config.guild_id],
-		options=[
-			create_option(
-				name="query",
-				description="Your search terms",
-				option_type=SlashCommandOptionType.STRING,
-				required=True,
-			)
-		],
-	)
-	async def xkcd_search(self, interaction: nextcord.Interaction, query: str):
-		await ctx.defer()
-		comic = self.xkcd_fetcher.search_relevant(query)
-		await ctx.send(embed=self.xkcd_embedder.gen_embed(comic))
+	@xkcd.subcommand(name="get", description="Gets a specific xkcd comic")
+	async def get(
+		self,
+		interaction: nextcord.Interaction,
+		number: int = nextcord.SlashOption(
+			description="The number of the comic to display"
+		),
+	):
+		"""Displays an xkcd comic by number"""
+		interaction.response.defer()
+		try:
+			comic = await self.xkcd_fetcher.get_comic_by_id(number)
+		except ConnectionError as e:
+			await interaction.response.send_message(e, ephemeral=True)
+			return
+		await interaction.response.send_message(
+			embed=self.xkcd_embedder.gen_embed(comic)
+		)
+
+	@xkcd.subcommand(name="search", description="Searches for a relevant xkcd comic")
+	async def search(self, interaction: nextcord.Interaction, query: str):
+		"""Searches for a relevant xkcd comic"""
+		interaction.response.defer()
+		try:
+			comic = await self.xkcd_fetcher.search_relevant(query)
+		except ConnectionError as e:
+			await interaction.response.send_message(e, ephemeral=True)
+			return
+		await interaction.response.send_message(
+			embed=self.xkcd_embedder.gen_embed(comic)
+		)
 
 
 def setup(bot: commands.Bot):
