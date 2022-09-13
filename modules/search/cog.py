755613491445d0530e9dbfc3c67822fd4500a11c
BenjiTusk <benjitusk1@gmail.com>
2022-09-13 18:23:06 -0400

    Migrated /search to Nextcord (#316)

--- modules/search/cog.py
+++ modules/search/cog.py
@@ -1,6 +1,7 @@
 from typing import List
 from ..error.friendly_error import FriendlyError
 from nextcord.ext import commands
+import nextcord
 from googlesearch import search
 import modules.search.search_functions as sf
 import config
@@ -13,34 +14,26 @@ class SearchCog(commands.Cog):
 		self.bot = bot
 		self.last_paragraph = {}
 
-	@cog_ext.cog_slash(
-		name="search",
-		description="Search the web for anything you want.",
-		guild_ids=[config.guild_id],
-		options=[
-			create_option(
-				name="query",
-				description="Your search query",
-				option_type=SlashCommandOptionType.STRING,
-				required=True,
-			),
-		],
-	)
+	@nextcord.slash_command(name="search", guild_ids=[config.guild_id])
 	async def search(self, interaction: nextcord.Interaction, query: str):
-		await ctx.defer()
-
+		"""Search the web for anything you want."""
+		if query == "who asked":
+			await interaction.send(
+				"After a long and arduous search, I have found the answer to your"
+				" question: Nobody. Nobody asked."
+			)
+			return
+		await interaction.response.defer()
 		links: List[str] = [link for link in search(query) if link.startswith("http")]
 		if not links:
-			raise FriendlyError("We searched far and wide, but nothing turned up.", ctx)
-
+			raise FriendlyError("No results found", interaction)
 		wiki_links = [link for link in links if "wikipedia.org" in link[:30]]
 		wiki_intro = (
 			sf.get_wiki_intro(wiki_links[0])
 			if wiki_links and wiki_links[0] != links[0]
 			else None
 		)
-
-		await ctx.send(content=sf.format_message(query, links[0], wiki_intro))
+		await interaction.send(sf.format_message(query, links[0], wiki_intro))
 
 
 # setup functions for bot
