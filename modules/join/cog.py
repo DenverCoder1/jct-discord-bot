4733fe5dee0f62f18335e7c0f1d1d779fa75f52b
BenjiTusk <benjitusk1@gmail.com>
2022-09-15 15:16:53 -0400

    Migrated /join command to Nextcord

--- modules/join/cog.py
+++ modules/join/cog.py
@@ -4,6 +4,7 @@ from database.campus import Campus
 from utils import embedder, utils
 from . import assigner
 from nextcord.ext import commands
+import nextcord
 import config
 
 
@@ -14,68 +15,40 @@ class JoinCog(commands.Cog):
 		self.bot = bot
 		self.assigner = None
 
-	@cog_ext.cog_slash(
-		name="join",
-		description=(
-			"Join command to get new users' information and place them in the right"
-			" roles."
-		),
-		guild_ids=[config.guild_id],
-		options=[
-			create_option(
-				name="first_name",
-				description="Your first name",
-				option_type=SlashCommandOptionType.STRING,
-				required=True,
-			),
-			create_option(
-				name="last_name",
-				description="Your last name",
-				option_type=SlashCommandOptionType.STRING,
-				required=True,
-			),
-			create_option(
-				name="campus",
-				description="Your campus (Lev or Tal)",
-				option_type=SlashCommandOptionType.STRING,
-				required=True,
-				choices=[
-					create_choice(name=campus.name, value=str(campus.id))
-					for campus in preloaded.campuses
-				],
-			),
-			create_option(
-				name="year",
-				description="Your year",
-				option_type=SlashCommandOptionType.STRING,
-				required=True,
-				choices=[
-					create_choice(name=f"Year {i}", value=str(i)) for i in range(1, 5)
-				],
-			),
-		],
-	)
+
+
+	@nextcord.slash_command("join", guild_ids=[config.guild_id])
 	@commands.has_role(utils.get_id("UNASSIGNED_ROLE"))
 	async def join(
 		self,
 		interaction: nextcord.Interaction,
 		first_name: str,
 		last_name: str,
-		campus: str,
-		year: str,
+		campus: int = nextcord.SlashOption(choices={
+			campus.name:campus.id
+			for campus in preloaded.campuses
+		}),
+		year: str = nextcord.SlashOption(choices=[str(i) for i in range(1, 5)]),
 	):
-		# TODO: campus and year should really take integer options, but mobile has a bug
-		await ctx.defer()
+		"""Join command to get new users' information and place them in the right roles.
+		
+			Args:
+				first_name (str): Your first name.
+				last_name (str): Your last name.
+				campus (str): Your campus (Lev or Tal).
+				year (int): The user's year.
+		"""
+		await interaction.response.defer()
 		await assigner.assign(
-			ctx.author,
+			interaction.user,
 			f"{first_name.title()} {last_name.title()}",
 			int(campus),
 			int(year),
 		)
-		await ctx.send(
+		await interaction.send(
 			embeds=[
 				embedder.embed_success(
-					title=f"**{ctx.author.display_name}** used **/{ctx.invoked_with}**"
+					title=f"**{interaction.user.display_name}** used **/{interaction.application_command.name}**"
 				)
 			]
 		)
