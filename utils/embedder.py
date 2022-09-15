653837a6c10b4454443ccefa5c951d7b77cd7d68
BenjiTusk <benjitusk1@gmail.com>
2022-09-15 16:50:28 -0400

    Updattd types in embedder.py for nextcord 2.2.0

--- utils/embedder.py
+++ utils/embedder.py
@@ -1,6 +1,5 @@
 import nextcord
 from typing import Optional, Union
-from nextcord.embeds import EmptyEmbed, _EmptyEmbed
 from utils import utils
 
 MAX_EMBED_DESCRIPTION_LENGTH = 4096
@@ -12,7 +11,7 @@ def embed_success(
 	title: str,
 	description: Optional[str] = None,
 	footer: Optional[str] = None,
-	url: Union[str, _EmptyEmbed] = EmptyEmbed,
+	url: Optional[str] = None,
 	image: Optional[str] = None,
 ) -> nextcord.Embed:
 	"""Embed a success message and an optional description, footer, and url"""
@@ -23,7 +22,7 @@ def embed_warning(
 	title: str,
 	description: Optional[str] = None,
 	footer: Optional[str] = None,
-	url: Union[str, _EmptyEmbed] = EmptyEmbed,
+	url: Optional[str] = None,
 	image: Optional[str] = None,
 ) -> nextcord.Embed:
 	"""Embed a warning message and an optional description, footer, and url"""
@@ -34,7 +33,7 @@ def embed_error(
 	title: str,
 	description: Optional[str] = None,
 	footer: Optional[str] = None,
-	url: Union[str, _EmptyEmbed] = EmptyEmbed,
+	url: Optional[str] = None,
 	image: Optional[str] = None,
 ) -> nextcord.Embed:
 	"""Embed an error message and an optional description, footer, and url"""
@@ -45,13 +44,15 @@ def build_embed(
 	title: str,
 	description: Optional[str] = None,
 	footer: Optional[str] = None,
-	url: Union[str, _EmptyEmbed] = EmptyEmbed,
+	url: Optional[str] = None,
 	colour: nextcord.Colour = nextcord.Colour.blurple(),
 	image: Optional[str] = None,
 ) -> nextcord.Embed:
 	"""Embed a message and an optional description, footer, and url"""
 	# create the embed
-	embed = nextcord.Embed(title=utils.trim(title, MAX_EMBED_FIELD_TITLE_LENGTH), url=url, colour=colour)
+	embed = nextcord.Embed(
+		title=utils.trim(title, MAX_EMBED_FIELD_TITLE_LENGTH), url=url, colour=colour
+	)
 	if description:
 		embed.description = utils.trim(description, MAX_EMBED_DESCRIPTION_LENGTH)
 	if footer:
