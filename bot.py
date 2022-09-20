6311f881ee840b8f2cdd342ca8e70acd99225d29
Abraham Murciano <abrahammurciano@gmail.com>
2022-09-20 10:05:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,6 @@ async def main():
 
 	activity = nextcord.Game("with students' patience")
 
-	# empty space effectively disables prefix since discord strips trailing spaces
 	bot = commands.Bot(intents=intents, activity=activity)
 
 	# Get the modules of all cogs whose directory structure is modules/<module_name>/cog.py
